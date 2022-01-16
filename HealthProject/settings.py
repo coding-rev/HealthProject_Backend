451de3ca9c3b3b644005f381d44ae527b057e3f0
codingrev <emmowu10@gmail.com>
2022-01-16 22:34:50 +0000

    app dockerized

--- HealthProject/settings.py
+++ HealthProject/settings.py
@@ -11,9 +11,6 @@ import dj_database_url
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 
-# Quick-start development settings - unsuitable for production
-# See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
-
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = os.environ.get("SECRET_KEY", "HealthProjecthjsagfhsaf-afhjdafh")
 
